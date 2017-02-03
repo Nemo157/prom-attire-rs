51bf4953f72e4e0207a3b2ac2b34625926e33bb7
Wim Looman <wim@nemo157.com>
2017-02-03 22:23:25 +0100

    make scope just 'attire'

--- prom-attire-bootstrap/src/lib.rs
+++ prom-attire-bootstrap/src/lib.rs
@@ -6,7 +6,7 @@ extern crate prom_attire_impl;
 #[proc_macro_derive(FromAttributesBootstrap)]
 pub fn app(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let config = prom_attire_impl::Config {
-        scope: Some("prom_attire"),
+        scope: Some("attire"),
     };
     let input = &input.to_string();
     prom_attire_impl::derive(input, config).unwrap().parse().unwrap()
