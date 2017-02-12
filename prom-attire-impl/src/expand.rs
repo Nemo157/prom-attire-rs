1a92099944fc7baedd209529e257dfd23af919e2
Wim Looman <wim@nemo157.com>
2017-02-12 12:33:46 +0100

    Move to returning errors instead of panicking

--- prom-attire-impl/src/expand.rs
+++ prom-attire-impl/src/expand.rs
@@ -4,6 +4,45 @@ use quote::{Tokens, ToTokens};
 use dissect::{Struct, Field, Wrapper, Ty, Lit};
 use Config;
 
+struct Context<'a> {
+    config: &'a Config<'a>,
+    strukt_ty: &'a syn::Ident,
+    /// A lifetime, either one from the struct or a new generated one
+    life: Tokens,
+    /// A lifetime bound, using the lifetime `life`
+    life_bound: Tokens,
+    /// A lifetime bound if the struct has a lifetime bound, if it is Some it
+    /// uses the lifetime from `life`
+    opt_life_bound: Option<Tokens>,
+    scope_lit: Tokens,
+    error_ty: syn::Ident,
+}
+
+impl<'a> Context<'a> {
+    fn new(strukt: &'a Struct, config: &'a Config) -> Context<'a> {
+        let opt_life_bound = strukt.lifetime.as_ref().map(|l| quote!(<#l>));
+        let life = strukt.lifetime
+            .as_ref()
+            .map(|l| quote!(#l))
+            .unwrap_or_else(|| quote!('a));
+        let life_bound = quote!(<#life>);
+
+        Context {
+            config: config,
+            strukt_ty: &strukt.ast.ident,
+            life: life,
+            life_bound: life_bound,
+            opt_life_bound: opt_life_bound,
+            scope_lit: match config.scope {
+                Some(ref scope) => quote!(Some(#scope)),
+                None => quote!(None),
+            },
+            error_ty: syn::Ident::new(strukt.ast.ident.as_ref().to_string() +
+                                      "FromAttributesError"),
+        }
+    }
+}
+
 fn setup_docs(field: &Field) -> Tokens {
     let ident = &field.ident;
     quote! {
@@ -57,67 +96,88 @@ fn write_field(field: &Field) -> Tokens {
     }
 }
 
-fn match_error(scope: &Option<&str>, ty: &Ty) -> Tokens {
-    if let Some(ref scope) = *scope {
-        quote! {
-            _ => panic!(
-                concat!(
-                    "Unexpected attribute literal '{:?}' for ",
-                    stringify!(#scope),
-                    "({}), expected ",
-                    stringify!(#ty)),
-                value,
-                ident.as_ref())
-        }
-    } else {
-        quote! {
-            _ => panic!(
-                concat!(
-                    "Unexpected attribute literal '{:?}' for {}, expected ",
-                    stringify!(#ty)),
-                value,
-                ident.as_ref())
+fn match_error(ctx: &Context, ty: &Ty) -> Tokens {
+    let scope_lit = &ctx.scope_lit;
+    let error_ty = &ctx.error_ty;
+    quote! {
+        _ => {
+            errors.push(#error_ty::LiteralTy {
+                value: value,
+                ty: stringify!(#ty),
+                scope: #scope_lit,
+                attr: ident.as_ref(),
+            });
+            continue;
         }
     }
 }
 
-fn match_parse(ty: &Ty) -> Tokens {
+fn match_parse(ctx: &Context, ty: &Ty) -> Tokens {
+    let scope_lit = &ctx.scope_lit;
+    let error_ty = &ctx.error_ty;
     match *ty {
         Ty::Literal(Lit::Str) => {
             quote! {
                 value.as_ref()
             }
         }
+
         Ty::Literal(Lit::Char) => {
             quote! {
                 if value.len() != 1 {
-                    panic!(
-                        "Parsing attribute value {:?} for {} failed: {}",
-                        value, ident.as_ref(), "expected one character");
+                    errors.push(#error_ty::Parsing {
+                        value: value,
+                        ty: stringify!(#ty),
+                        scope: #scope_lit,
+                        attr: ident.as_ref(),
+                        err: Box::new(#error_ty::ParsingChar),
+                    });
+                    continue;
                 }
                 value.chars().next().unwrap()
             }
         }
+
         Ty::Literal(Lit::ByteStr) => {
             quote! {
-                assert!(::std::ascii::AsciiExt::is_ascii(value.as_str()));
-                value.as_bytes()
+                if ::std::ascii::AsciiExt::is_ascii(value.as_str()) {
+                    value.as_bytes()
+                } else {
+                    errors.push(#error_ty::Parsing {
+                        value: value,
+                        ty: stringify!(#ty),
+                        scope: #scope_lit,
+                        attr: ident.as_ref(),
+                        err: Box::new(#error_ty::ParsingByteStr),
+                    });
+                    continue;
+                }
             }
         }
+
         ref ty => {
             quote! {
-                <#ty as ::std::str::FromStr>::from_str(value)
-                    .unwrap_or_else(|err| {
-                        panic!(
-                            "Parsing attribute value {:?} for {} failed: {}",
-                            value, ident.as_ref(), err)
-                    })
+                match <#ty as ::std::str::FromStr>::from_str(value) {
+                    Ok(value) => value,
+                    Err(err) => {
+                        errors.push(#error_ty::Parsing {
+                            value: value,
+                            ty: stringify!(#ty),
+                            scope: #scope_lit,
+                            attr: ident.as_ref(),
+                            err: Box::new(err),
+                        });
+                        continue;
+                    }
+                }
             }
         }
     }
 }
 
-fn match_literal(ty: &Ty, lit: Lit) -> Tokens {
+fn match_literal(ctx: &Context, ty: &Ty, lit: Lit) -> Tokens {
+    let scope_lit = &ctx.scope_lit;
+    let error_ty = &ctx.error_ty;
     match lit {
         Lit::Bool => {
             quote! { ::syn::Lit::Bool(value) => { value } }
@@ -129,7 +189,7 @@ fn match_literal(ty: &Ty, lit: Lit) -> Tokens {
             quote! { ::syn::Lit::Int(value, _) => { value as #ty } }
         }
         Lit::Str => {
-            // Handle as a parse
+            // Handled as a parse
             quote!()
         }
         Lit::ByteStr => {
@@ -138,19 +198,27 @@ fn match_literal(ty: &Ty, lit: Lit) -> Tokens {
         Lit::Float(_) => {
             quote! {
                 ::syn::Lit::Float(ref value, _) => {
-                    <#ty as ::std::str::FromStr>::from_str(value.as_str())
-                        .unwrap_or_else(|err| {
-                            panic!(
-                                "Parsing attribute value {:?} for {} failed: {}",
-                                value, ident.as_ref(), err)
-                        })
+                    match <#ty as ::std::str::FromStr>::from_str(value.as_str()) {
+                        Ok(value) => value,
+                        Err(err) => {
+                            errors.push(#error_ty::Parsing {
+                                value: value,
+                                ty: stringify!(#ty),
+                                scope: #scope_lit,
+                                attr: ident.as_ref(),
+                                err: Box::new(err),
+                            });
+                            continue;
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-fn match_write(ident: &syn::Ident, ty: &Wrapper) -> Tokens {
+fn match_write(field: &Field, ty: &Wrapper) -> Tokens {
+    let ident = &field.ident;
     match *ty {
         Wrapper::Vec(_) => {
             quote! {
@@ -170,9 +238,9 @@ fn match_write(ident: &syn::Ident, ty: &Wrapper) -> Tokens {
     }
 }
 
-fn match_special(ident: &syn::Ident, ty: &Wrapper) -> Tokens {
-    let ident_str = ident.as_ref();
-    let write = match_write(ident, ty);
+fn match_special(field: &Field, ty: &Wrapper) -> Tokens {
+    let ident_str = field.ident.as_ref();
+    let write = match_write(field, ty);
     match *ty.inner() {
         Ty::Literal(Lit::Bool) => {
             quote! {
@@ -187,15 +255,17 @@ fn match_special(ident: &syn::Ident, ty: &Wrapper) -> Tokens {
     }
 }
 
-fn match_field(field: &Field, config: &Config) -> Tokens {
+fn match_field(ctx: &Context, field: &Field) -> Tokens {
     let ident = &field.ident;
     let ident_str = ident.as_ref();
-    let error = match_error(&config.scope, field.ty.inner());
-    let parse = match_parse(field.ty.inner());
-    let literal =
-        field.ty.inner().lit().map(|lit| match_literal(field.ty.inner(), lit));
-    let write = match_write(ident, &field.ty);
-    let special = match_special(ident, &field.ty);
+    let error = match_error(ctx, field.ty.inner());
+    let parse = match_parse(ctx, field.ty.inner());
+    let literal = field.ty
+        .inner()
+        .lit()
+        .map(|lit| match_literal(ctx, field.ty.inner(), lit));
+    let write = match_write(field, &field.ty);
+    let special = match_special(field, &field.ty);
     quote! {
         ::syn::MetaItem::NameValue(ref ident, ref value)
             if ident.as_ref() == #ident_str => {
@@ -213,10 +283,10 @@ fn match_field(field: &Field, config: &Config) -> Tokens {
 }
 
 fn match_loop<I: Iterator<Item = Tokens>>(
-    matches: I,
-    config: &Config
+    ctx: &Context,
+    matches: I
 ) -> Tokens {
-    if let Some(scope) = config.scope {
+    if let Some(scope) = ctx.config.scope {
         quote! {
             for attr in attrs {
                 if let ::syn::MetaItem::List(ref ident, ref values) = attr.value {
@@ -253,30 +323,128 @@ fn match_loop<I: Iterator<Item = Tokens>>(
 }
 
 pub fn expand(strukt: &Struct, config: &Config) -> Tokens {
-    let ident = &strukt.ast.ident;
+    let ctx = Context::new(strukt, config);
+
     let setup_fields = strukt.fields
         .iter()
         .map(setup_field)
         .chain(strukt.docs.as_ref().map(setup_docs));
     let field_matches = strukt.fields
         .iter()
-        .map(|field| match_field(field, config));
-    let match_loop = match_loop(field_matches, config);
+        .map(|field| match_field(&ctx, field));
+    let match_loop = match_loop(&ctx, field_matches);
     let write_fields =
         strukt.fields.iter().chain(&strukt.docs).map(write_field);
-    let a = if strukt.lifetime.is_some() {
-        quote!(<'a>)
-    } else {
-        quote!()
-    };
+
+    let Context { ref strukt_ty,
+                  ref error_ty,
+                  ref life,
+                  ref life_bound,
+                  ref opt_life_bound,
+                  .. } = ctx;
+
     quote! {
-        impl<'a> From<&'a [::syn::Attribute]> for #ident#a {
-            fn from(attrs: &[::syn::Attribute]) -> #ident {
+        #[allow(dead_code)]
+        #[derive(Debug)]
+        enum #error_ty#life_bound {
+            LiteralTy {
+                value: &#life ::syn::Lit,
+                ty: &'static str,
+                scope: Option<&'static str>,
+                attr: &#life str,
+            },
+
+            Parsing {
+                value: &#life str,
+                ty: &'static str,
+                scope: Option<&'static str>,
+                attr: &#life str,
+                err: Box<::std::error::Error + #life>,
+            },
+
+            /// This is an internal error that will only be returned as part of
+            /// the `err` field on `Parsing`
+            ParsingChar,
+
+            /// This is an internal error that will only be returned as part of
+            /// the `err` field on `Parsing`
+            ParsingByteStr,
+        }
+
+        impl#life_bound ::std::fmt::Display for #error_ty#life_bound {
+            fn fmt(&self, mut w: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+                match *self {
+                    #error_ty::LiteralTy { ref value, ref ty, ref scope, ref attr } => {
+                        write!(
+                            w,
+                           "Unexpected attribute literal {:?} for {}, expected a {}",
+                            value,
+                            scope.map(|s| format!("{}({})", s, attr)).unwrap_or_else(|| attr.to_string()),
+                            ty)
+                    }
+
+                    #error_ty::Parsing { ref value, ref ty, ref scope, ref attr, ref err } => {
+                        write!(
+                            w,
+                           "Parsing attribute value {:?} into a {} for {} failed: {}",
+                            value,
+                            scope.map(|s| format!("{}({})", s, attr)).unwrap_or_else(|| attr.to_string()),
+                            ty,
+                            err)
+                    }
+
+                    #error_ty::ParsingChar => {
+                        write!(w, "expected one character")
+                    }
+
+                    #error_ty::ParsingByteStr => {
+                        write!(w, "non-ascii character")
+                    }
+                }
+            }
+        }
+
+        impl#life_bound ::std::error::Error for #error_ty#life_bound {
+            fn description(&self) -> &str {
+                match *self {
+                    #error_ty::LiteralTy { .. } => "Unexpected attribute literal",
+                    #error_ty::Parsing { .. } => "Parsing attribute value failed",
+                    #error_ty::ParsingChar => "expected one character",
+                    #error_ty::ParsingByteStr => "non-ascii character",
+                }
+            }
+
+            fn cause(&self) -> Option<&::std::error::Error> {
+                match *self {
+                    #error_ty::LiteralTy { .. } => None,
+                    #error_ty::Parsing { ref err, .. } => Some(&**err),
+                    #error_ty::ParsingChar => None,
+                    #error_ty::ParsingByteStr => None,
+                }
+            }
+        }
+
+        impl#opt_life_bound #strukt_ty#opt_life_bound {
+            #[allow(unused_mut)]
+            fn try_from(attrs: &[::syn::Attribute])
+                -> ::std::result::Result<#strukt_ty, ::std::vec::Vec<#error_ty>>
+            {
+                let mut errors = vec![];
                 #(#setup_fields)*
                 #match_loop
-                #ident {
-                    #(#write_fields)*
+                if errors.is_empty() {
+                    Ok(#strukt_ty {
+                        #(#write_fields)*
+                    })
+                } else {
+                    Err(errors)
                 }
+            }
+        }
+
+        impl<#life> From<&#life [::syn::Attribute]> for #strukt_ty#opt_life_bound {
+            fn from(attrs: &[::syn::Attribute]) -> #strukt_ty {
+                #strukt_ty::try_from(attrs).unwrap()
             }
         }
     }
