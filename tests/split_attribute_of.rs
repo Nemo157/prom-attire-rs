52264a38d470fb4d92d4ce9c981d3456638a33b0
Wim Looman <wim@nemo157.com>
2017-03-05 12:47:00 +0100

    Add another couple test cases

--- tests/split_attribute_of.rs
+++ tests/split_attribute_of.rs
@@ -63,3 +63,39 @@ fn split_attribute_of_children() {
     assert_eq!(attrs.d, Some("d"));
     assert_eq!(attrs.e, None);
 }
+
+#[test]
+fn split_attribute_and_normal() {
+    #[derive(PromAttire)]
+    struct A<'a> {
+        #[attire(split_attribute_of = "b")]
+        c: Option<&'a str>,
+        #[attire(attribute = "c")]
+        d: Option<&'a str>,
+    }
+    let input = quote! {
+        #[b = "b"]
+        #[c = "c"]
+        struct S {}
+    };
+    let ast = syn::parse_derive_input(input.as_str()).unwrap();
+    let attrs = A::from(ast.attrs.as_slice());
+    assert_eq!(attrs.c, Some("b"));
+    assert_eq!(attrs.d, Some("c"));
+}
+
+#[test]
+fn split_attribute_with_rename() {
+    #[derive(PromAttire)]
+    struct A<'a> {
+        #[attire(split_attribute_of = "b", attribute = "d")]
+        c: Option<&'a str>,
+    }
+    let input = quote! {
+        #[b(d = "b")]
+        struct S {}
+    };
+    let ast = syn::parse_derive_input(input.as_str()).unwrap();
+    let attrs = A::from(ast.attrs.as_slice());
+    assert_eq!(attrs.c, Some("b"));
+}
